d2ac3d2ec1b16f932e576b3718d50cdb38997056
Hardik Sharma <146644901+hardik1144@users.noreply.github.com>
2024-03-31 22:47:56 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,136 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <div className="main-div">
+          <div className="sub-div">
+            <h1 className="sport">Sport</h1>
+            <div className="box-main">
+            <div className="box">
+                <img  className="img" src="https://s3-alpha-sig.figma.com/img/ceed/8067/dc33760174d3f67737f6e318d0f118ff?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=F2XxKQcUNpnjUfqPSEoBDs1fA2T2ynT30pmeBbbJh8ADDuqsounqPy2h5snUTaNLrXmBSn3WLCzp0n2OT-QW-YTvS-z2mJHJ6qjt2YKrUp47e-cjjKeMCUFbLFl1st3rJjLbv0LslNuyK2PcJ7sfJvoojd2jJrTkPwtCe~fh8aXrwNSkOfd6fBp5EH6WVggIjH~17u23Nxit7mJ3gnCJVAFnyCwu7GXicoEtWv3OQ6g4X7r91yeBwHIuSi~KlMIqvE57pklcMv80Q6TC5meP~-Tk70JwQVh9kJFCBf5kGia04Ta-8Jgjo8h6GaYCkbtJuphhniHWXT4dnVFsj7xMmQ__" alt="" />
+                <h2 className="text">Sacramento River Cats</h2>
+               <div className="box-div-cont">
+                 <div className="text-div">
+                  <div className="box-first">total events</div>
+                  <div className="box-second">total events</div>
+                </div>
+                <div className="text-div">
+                  <div className="box-first">total events</div>
+                  <div className="box-second">total events</div>
+                </div>
+               </div>
+              
+              </div>
+
+              <div className="box">
+                <img  className="img" src="https://s3-alpha-sig.figma.com/img/a24a/d1e0/76e2b366b2456bbd169b0c3c9525252c?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fnhdy7dQOQtLfNEts8Y48S1XXIo78WTAF8NMCby7P4sjC6OTdliMhF0d0tAj~lmWSrjvqUtg7~AfR9Cu7UzkprB34TEfUwdP17WZw6ak-PHZ5KUq43yWAfJQbKTahlsvrfN9t8YcNsp538gquzN~Q4gOC5DyWALE0EhpvBxb4NimSNiTG4WjdxfSwz5eOHvqWLk57~o3erxXoDjv0quRDx7PvAKyLnzLkW7L-2ivUCwu1QnVd4ZEDQez5aU8Gf0CRrwva7Tsoj5ZuQdw1yBolX9aJ1deMVeCinrZUWkKaLkTntZYMMP5GuIM9pr2sCp3MZiCGDZnwtQBcQwh0gS0TQ__" alt="" />
+                <h2 className="text">Sacramento River Cats</h2>
+               <div className="box-div-cont">
+                 <div className="text-div">
+                  <div className="box-first">total events</div>
+                  <div className="box-second">total events</div>
+                </div>
+                <div className="text-div">
+                  <div className="box-first">total events</div>
+                  <div className="box-second">total events</div>
+                </div>
+               </div>
+              
+              </div>
+
+              <div className="box">
+                <img  className="img" src="https://s3-alpha-sig.figma.com/img/b290/2f7a/b2fcf21c696811f745109da24a740d73?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SL2NyvBA-RmV9CySCr0HqaIDFJPJfx24z5ukOpUhta6I9GYXc65jVXyjYdWiNUOcTOt2pbLQb2rK1ME8yx1Xkvvw14syZfWGf9I-Bx~7rNiIGN4EetaP9GdAdoJpMJP2cYcwI2HwhG5p4P~LHqFKou0RO~ZEZcMOPJxJct8ini~YOjXlxwbDdvfk6v8UGdmtaXuR43hC36YWrAdNVcfWnkNY3LrxnefSV6pJmr6-M9fOwGOpWFqWE08XG9YjfexBZQmEM7MLfm338bYXaIXLtAwQGvnhqrrmwCl4wHPPc95mDocM14BBhj8aUaDvPMpBAWUKhw2OYXVoUBbvcNditw__" alt="" />
+                <h2 className="text">Sacramento River Cats</h2>
+               <div className="box-div-cont">
+                 <div className="text-div">
+                  <div className="box-first">total events</div>
+                  <div className="box-second">total events</div>
+                </div>
+                <div className="text-div">
+                  <div className="box-first">total events</div>
+                  <div className="box-second">total events</div>
+                </div>
+               </div>
+              
+              </div>
+
+              <div className="box">
+                <img  className="img" src="https://s3-alpha-sig.figma.com/img/c724/731e/f71497e0e3af85c77fa47046bb1f23cb?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YfHZuev7WUagkCJeDUVdHbkG5bqC2YmjXKJSZLKbyymg8PLUCF1d-3ITeG2OSpcKgHjVq3lbGf7eLgT0-RWii9vAbSSrltvD7RI88jX3A1p0QZTvL~643uJOzylJw00w1DWQKzrTEHLkK2D4jxJ4n3C9D03B-Odrn9zVjUWBi9mqnU5op991kONICO-AT7eCWb2APcuZxUGYnj8xpoj5L5modFS2ABEy2X1gaT0HXJZoVu2276JkODvrtjnC3F4~lM3f7rLQQam1iI8a3frGTCuFQfbXM3VAQ7nfBJDVDCcyeJcyc4m1oPldx2ONzo2CGn5jRfonRO5RlD-OHbierA__" alt="" />
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                
+                
+                
+                {/* <h2 className="text">Sacramento River Cats</h2>
+               <div className="box-div-cont">
+                 <div className="text-div">
+                  <div className="box-first">total events</div>
+                  <div className="box-second">total events</div>
+                </div>
+                <div className="text-div">
+                  <div className="box-first">total events</div>
+                  <div className="box-second">total events</div>
+                </div>
+               </div> */}
+              
+              </div>
+            </div>
+            <div className="second-box">
+              <h1>
+              Collection Spotlight
+              </h1>
+              <p>Discover extraordinary moments with our Spotlight Collection metatickets—exclusive access to premium events for an unforgettable experience. Grab yours today!</p>
+               <div>
+                <div className="second-flex">
+
+
+               <div className="second-box-main">
+                  <img className="img" src="https://s3-alpha-sig.figma.com/img/bc7b/d2d1/a2c7e5def5b347470305864869a894b6?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YxtMQ-umkbEHlxMyMnstAs6rYbIBMoz6Y6e6X~XvOsr-rAQ2g4qOWln3w~caMGqtbtAtbCjyj-yjVTMTm1mZVFfJBk5KOXa491~GuF-sCId9i622FHtykZ-nY9kpx54GqgSCB2nGFH3HeaHmDDrU0aEinZpFFnqbmnXRqB9agt6U5PFCW70dobn1FS2-uCCVjf-JcgJzpDyt6xoO-4xWk6IWNKkJCo1K7Ad4KoEpKKDiFYyDhsXOKGZrevPj~1tdFMRrtdkTS8ckyL82IMwnzlMMtKVgab1EpeEiBiF-BW88PwJ0towH4BaKyuBx~E-cLgN0lW8hacJNXlAv-u1nFA__" alt="" />
+                  <div>
+                    <p>Las Vegas Aviators</p>
+                    <p>Oct 15 Sun 4:30 PM</p>
+                    <p>Las Vegas Ballpark, Las Vegas, Nevada</p>
+                  </div>
+                  <div className="black-box">
+                    <h1>Orange Collection</h1>
+                  </div>
+                </div>
+
+
+                <div className="second-box-main">
+                  <img className="img" src="https://s3-alpha-sig.figma.com/img/3de4/4e83/b6e3aeda879dc6f22f94711c788789b5?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Ptz8aSbr1X2gpkQvNp75uy5Qqc3G8thxATUNJrSlH2UpQlPHInbDDZzPDm06nh~Nz5I1JJ4DJV7c8-QhyhII56mMhaJJpXXDp1PCUpSFQ4e8Ye14EdQY91xENMHr1f0JdG2SSkVcxAMgzFhchgpPmcJgVvZX42PQo9HDwrB-Mu9YzL8HDg5lqq592kuVfpcoCk2mmirfFLs7nHYKlxKizeHFC38nNStqJTHYUJHGziz0RNo2oRTX9lYT5MCG0g7fSSu968zB9IjW7Xu5z337Wt3fLdxiQu12YbUec3D-iYVeMH-6xRitiF8XgVwKniDwWIEs6yrvu122pOCHDB~P6g__" alt="" />
+                  <div>
+                    <p>Sacramento River Cats</p>
+                    <p>Oct 15 Sun 4:30 PM</p>
+                    <p>Sutter Health Park, Sacramento, California</p>
+                  </div>
+                  <div className="black-box">
+                    <h1>Take Flight Collection</h1>
+                  </div>
+                </div>
+
+
+                <div className="second-box-main">
+                  <img className="img" src="https://s3-alpha-sig.figma.com/img/bc7b/d2d1/a2c7e5def5b347470305864869a894b6?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YxtMQ-umkbEHlxMyMnstAs6rYbIBMoz6Y6e6X~XvOsr-rAQ2g4qOWln3w~caMGqtbtAtbCjyj-yjVTMTm1mZVFfJBk5KOXa491~GuF-sCId9i622FHtykZ-nY9kpx54GqgSCB2nGFH3HeaHmDDrU0aEinZpFFnqbmnXRqB9agt6U5PFCW70dobn1FS2-uCCVjf-JcgJzpDyt6xoO-4xWk6IWNKkJCo1K7Ad4KoEpKKDiFYyDhsXOKGZrevPj~1tdFMRrtdkTS8ckyL82IMwnzlMMtKVgab1EpeEiBiF-BW88PwJ0towH4BaKyuBx~E-cLgN0lW8hacJNXlAv-u1nFA__" alt="" />
+                  <div>
+                    <p>Las Vegas Aviators</p>
+                    <p>Oct 15 Sun 4:30 PM</p>
+                    <p>Las Vegas Ballpark, Las Vegas, Nevada</p>
+                  </div>
+                  <div className="black-box">
+                    <h1>Take Flight Collection</h1>
+                  </div>
+                </div>
+                </div>
+
+
+                
+               </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
